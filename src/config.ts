639d70a97f0d61f8fb1a1221eba4c34f901dbea1
stas_fr <sfworking@yandex.ru>
2025-07-19 21:59:22 +0300

    feat: add API_URL to config

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,10 @@
-export const MILVUS_ADDRESS = '192.168.1.105:19530';
+export const API_URL = '192.168.1.102';
+
+export const MILVUS_ADDRESS = `${API_URL}:19530`;
 export const COLLECTION_NAME = 'test_rag_collection_with_date';
 export const VECTOR_DIMENSION = 1024;
 
-export const EMBEDDING_SERVICE_URL = 'http://192.168.1.105:1234/v1/embeddings';
+export const EMBEDDING_SERVICE_URL = `${API_URL}/v1/embeddings`;
 export const EMBEDDING_MODEL_NAME = 'text-embedding-intfloat-multilingual-e5-large-instruct';
 
 export const QUERY_TEXT = 'Павлино, стрельба, убийство';
