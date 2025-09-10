6e6a1dc04080e3e8da89e5a4e6c4fc96f5d546f1
stas_fr <sfworking@yandex.ru>
2025-09-10 21:16:50 +0300

    feat: Reorder module router imports and inclusions

--- src/router.py
+++ src/router.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter
 
 from src.modules.db.router import router as db_router
-from src.modules.parsers.router import router as parsers_router
 from src.modules.tg_exports.router import router as tg_exports_router
+from src.modules.parsers.router import router as parsers_router
 
 router = APIRouter(
     prefix="/api",
 )
 
 router.include_router(db_router)
-router.include_router(parsers_router)
 router.include_router(tg_exports_router)
+router.include_router(parsers_router)
